2684b6ac6e26cff9877d69385fb558582044dd7b
NILOTPAL MRINAL <nilotpal.avanti@gmail.com>
2020-07-15 12:54:23 +0530

    Update code.py

--- chapter-1/Problems/code.py
+++ chapter-1/Problems/code.py
@@ -1,3 +1,12 @@
 import math
 
-n = 
+n = int(input("Enter the number of seconds: "))
+# all the duration can be converted into the seconds by simply multiplying by corresponding factors.
+
+print(math.log2(n))
+print(math.factorial(n))
+print(math.sqrt(n))
+print(n*n)
+print(n**3)
+print(2**n)
+print(n*math.log2(n))
