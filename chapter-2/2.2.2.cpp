9ef2d2734db16b01f8aebff0501730744bc9f9b1
NILOTPAL MRINAL <nilotpal.avanti@gmail.com>
2020-07-16 13:10:22 +0530

    Update 2.2.2.cpp

--- chapter-2/2.2.2.cpp
+++ chapter-2/2.2.2.cpp
@@ -2,5 +2,17 @@
 // Program to implement selection sort in O(n^2) complexity.
 int selectiosort(int arr[])
 {
-  
+  int i,j,min;
+  for(i = 1; i < n ; i++) // It goes only till n-1 because the last element is expected to be sorted(of course trivially)
+  {
+    min = arr[i];
+    for( j = i+1; j<=n; j++   )
+    {
+      if(arr[j] < min)
+      {
+        min = arr[j];
+        arr[i] = min;
+      }
+    }
+  }
 }
